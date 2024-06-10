5b05597c7ccc078398e2598070c889d9c2e5435f
esviren <st-rodriguez@hotmail.com>
2024-06-10 11:05:52 -0500

    create menu and cards categoriescomponet

---
+++ components/Bakery.vue
@@ -0,0 +1,10 @@
+<template>
+    <h2>Pet Bakery FooD</h2>
+    <div class="flex flex-nowrap overflow-x-auto  my-8">
+        <Card v-for="card in productA" :key="card.id" :="card"> </Card>
+    </div>   
+</template>
+
+<script setup lang="ts">
+    import { productA , productB } from '~/data/product-es.json'
+</script>
---
+++ components/Frozen.vue
@@ -0,0 +1,11 @@
+<template>
+    <h2>Pet Frozen Food</h2>
+    <div class="flex flex-nowrap overflow-x-auto  my-8">
+        <Card v-for="card in productB" :key="card.id" :="card"> </Card>
+    </div>
+    
+</template>
+
+<script setup lang="ts">
+    import { productA , productB } from '~/data/product-es.json'
+</script>
